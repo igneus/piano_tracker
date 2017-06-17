f0dbf957fc6070c69a66470647453392b6c9f160
Jakub Pavlík <severus@post.cz>
2017-06-17 11:22:22 +0200

    fit in as many keys as necessary

--- piano_tracker/graphic.py
+++ piano_tracker/graphic.py
@@ -9,45 +9,68 @@ def generate(stats, filename):
 
     draw = ImageDraw.Draw(image)
 
-    draw_keyboard(draw, size[0], KeyHeatmap(stats['keys']))
+    keys = stats['keys'].keys()
+    key_range = (min(keys), max(keys))
+    heatmap = KeyHeatmap(stats['keys'])
+    KeyboardDraw(key_range, size[0])(draw, heatmap)
 
     image.save(filename)
 
 def default_colour_func(key, default):
     return default
 
-def draw_keyboard(draw, width, colour_func=default_colour_func):
-    w = 20
-    h = 5 * w
+class KeyboardDraw(object):
+    """ draws part of a keyboard """
 
-    midi_key = lambda x: x + 24
+    def __init__(self, key_range, max_width):
+        self._min_key = key_range[0]
+        if self._black_key(self._min_key):
+            self._min_key -= 1
 
-    # white keys
-    key = 0
-    for i, x in enumerate(xrange(0, width - w, w)):
-        step = i % 7
-        draw.rectangle([x, 0, x + w, h], colour_func(midi_key(key), '#fff'), '#000')
+        self._max_key = key_range[1]
+        if self._black_key(self._max_key):
+            self._max_key += 1
 
-        if step == 2 or step == 6:
-            key += 1
-        else:
-            key += 2
+        self._max_width = max_width
 
-    # black keys
-    key = 0
-    for i, x in enumerate(xrange(0, width - w, w)):
-        step = i % 7
-        if step == 0 or step == 3:
-            key += 1
-            continue
+    def __call__(self, draw, colour_func=default_colour_func):
+        width = self._max_width
 
-        draw.rectangle([x - w/4, 0, x + w/4, h/2], colour_func(midi_key(key), '#000'), '#000')
+        # white key dimensions
+        w = self._max_width / self._white_keys_in_range()
+        w = min(w, 40)
+        h = 5 * w
 
-        key += 2
+        # white keys
+        i = 0
+        for key in xrange(self._min_key, self._max_key + 1):
+            x = i * w
+            if not self._black_key(key):
+                draw.rectangle([x, 0, x + w, h], colour_func(key, '#fff'), '#000')
+                i += 1
+
+        # black keys
+        i = 0
+        for key in xrange(self._min_key, self._max_key + 1):
+            x = i * w
+            if self._black_key(key):
+                draw.rectangle([x - w/4, 0, x + w/4, h/2], colour_func(key, '#000'), '#000')
+            else:
+                i += 1
+
+    def _black_key(self, key):
+        return (key % 12) in (1, 3, 6, 8, 10)
+
+    def _white_keys_in_range(self):
+        whites = 0
+        for i in xrange(self._min_key, self._max_key + 1):
+            if not self._black_key(i):
+                whites += 1
+        return whites
 
 class KeyHeatmap(object):
     """
-    callable which can be used as colouring callback for draw_keyboard;
+    callable which can be used as colouring callback for KeyboardDraw;
     generates heatmap-like colours based on a dict mapping
     key ids to hit counts
     """
