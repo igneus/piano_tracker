03e28b179576b1743e22d5f668666fa6909ca3ae
Jakub Pavlík <severus@post.cz>
2017-06-21 12:51:36 +0200

    intensity graph: use average values per second

--- piano_tracker/sampled_stats.py
+++ piano_tracker/sampled_stats.py
@@ -3,6 +3,7 @@ import sqlite3
 class SampledStats:
     columns = [
         ['id', 'INTEGER PRIMARY KEY'],
+        ['created_at', 'TEXT'],
         ['notes_playing', 'INTEGER'],
         ['recent_notes', 'INTEGER'],
     ]
@@ -13,12 +14,14 @@ class SampledStats:
         self.db.execute('CREATE TABLE samples (%s)' % column_spec)
 
     def push(self, data):
-        values = tuple(map(lambda x: data[x[0]], self.columns[1:]))
+        values = tuple(map(lambda x: data[x[0]], self._metric_columns()))
         placeholders = ','.join(['?'] * len(values))
-        self.db.execute('INSERT INTO samples VALUES (%s)' % placeholders, values)
+        self.db.execute("INSERT INTO samples VALUES (datetime('now'), %s)" % placeholders, values)
 
     def results(self):
-        items = self.db.execute('SELECT * FROM samples')
+        cols = ', '.join(['avg(%s)' % c for c in self._metric_column_names()])
+        group_by = "strftime('%Y-%m-%d %H:%M:%S', created_at)"
+        items = self.db.execute('SELECT %s FROM samples GROUP BY %s ORDER BY created_at ASC' % (cols, group_by))
         intensity = [self._intensity(i) for i in items]
 
         while len(intensity) > 2 and intensity[0] == 0 and intensity[1] == 0:
@@ -31,3 +34,10 @@ class SampledStats:
     def _intensity(self, record):
         # counts in polyphony and speed
         return record[0] + record[1]
+
+    def _metric_columns(self):
+        return self.columns[2:]
+
+    def _metric_column_names(self):
+        for i in self._metric_columns():
+            yield i[0]
